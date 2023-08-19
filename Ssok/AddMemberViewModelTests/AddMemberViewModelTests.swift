a23c6e2f13512c19e04e2f71886fad69af68a8a4
Mini <minnnidev@gmail.com>
2023-08-19 19:44:39 +0900

    Add: add AddMemberViewModelTC files

---
+++ Ssok/AddMemberViewModelTests/AddMemberViewModelTests.swift
@@ -0,0 +1,24 @@
+//
+//  AddMemberViewModelTests.swift
+//  SsokTests
+//
+//  Created by 김민 on 2023/08/19.
+//
+
+import XCTest
+@testable import Ssok
+
+final class AddMemberViewModelTests: XCTestCase {
+
+    private var addMemberViewModel: AddMemberViewModel!
+
+    override func setUpWithError() throws {
+        addMemberViewModel = AddMemberViewModel()
+    }
+
+    override func tearDownWithError() throws {
+        addMemberViewModel = nil
+    }
+
+    
+}
